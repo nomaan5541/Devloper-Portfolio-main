af6a23dea6e31e4c487d2286442fa6217a79b260
virus_boss <90496180+nomaan5541@users.noreply.github.com>
2025-12-07 23:09:26 +0530

    Update roleScript.js

--- roleScript.js
+++ roleScript.js
@@ -1,13 +1,15 @@
-var typeData = new Typed(".role", {
-  strings: [
-    "a Futuristic Developer",
-    "an AI Specialist",
-    "a Cybersecurity Expert",
-    "a Full Stack Developer",
-    "Nomaan Khan",
-  ],
-  loop: true,
-  typeSpeed: 100,
-  backSpeed: 80,
-  backDelay: 1000,
+document.addEventListener('DOMContentLoaded', function () {
+  var typeData = new Typed(".role", {
+    strings: [
+      "a Futuristic Developer",
+      "an AI Specialist",
+      "a Cybersecurity Expert",
+      "a Full Stack Developer",
+      "Nomaan Khan",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
 });
