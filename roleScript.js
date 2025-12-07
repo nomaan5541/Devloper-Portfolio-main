a21df4c9321249f92fea25d5945eaaacba4efb88
nomaan5541 <nomaankhannfs2@gmail.com>
2025-12-07 16:26:17 +0530

    Initial commit

---
+++ roleScript.js
@@ -0,0 +1,13 @@
+var typeData = new Typed(".role", {
+  strings: [
+    "a Futuristic Developer",
+    "an AI Specialist",
+    "a Cybersecurity Expert",
+    "a Full Stack Developer",
+    "Nomaan Khan",
+  ],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1000,
+});
